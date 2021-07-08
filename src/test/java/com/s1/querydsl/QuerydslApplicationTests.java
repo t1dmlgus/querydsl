caba8fe197492b3ea377278b1eca96c2addf06a6
t1dmlgus <dmlgusgngl@gmail.com>
2021-07-08 11:49:45 +0900

    Initial commit

---
+++ src/test/java/com/s1/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
