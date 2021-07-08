f5fbeeaad7ca401f6fbfb14eb63693d480f5ca11
t1dmlgus <dmlgusgngl@gmail.com>
2021-07-08 12:25:40 +0900

    Jpql, Querydsl 비교

--- src/test/java/com/s1/querydsl/domain/member/MemberTest.java
+++ src/test/java/com/s1/querydsl/domain/member/MemberTest.java
@@ -1,6 +1,9 @@
 package com.s1.querydsl.domain.member;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.s1.querydsl.domain.team.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +15,8 @@ import javax.persistence.TypedQuery;
 
 import java.util.List;
 
+import static com.s1.querydsl.domain.member.QMember.member;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,9 +26,14 @@ class MemberTest {
     @Autowired
     EntityManager em;
 
-    @Test
-    public void 관계_테스트() throws Exception{
-        //given
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+
+        queryFactory = new JPAQueryFactory(em);
+
+
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -45,6 +55,13 @@ class MemberTest {
         em.clear();     // 영속성 컨텍스트를 완전히 초기화, 캐시같은거 다 날라간다.
 
 
+    }
+
+
+    @Test
+    public void 관계_테스트() throws Exception{
+        //given
+
         //when
         List<Member> members =
                 em.createQuery("select m from Member m", Member.class)
@@ -55,9 +72,39 @@ class MemberTest {
             System.out.println("member.getTeam() = " + member.getTeam());
         }
 
-        //then
     }
 
+    @Test
+    public void 쿼리_JPQL() throws Exception{
+
+        // member1을 찾아라.
+        String qlString = "select m from Member m "
+                + "where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        // then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+    
+    @Test
+    public void 쿼리_Querydsl() throws Exception{
+        // member1을 찾아라.
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        //then
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+    
 
 }
 
